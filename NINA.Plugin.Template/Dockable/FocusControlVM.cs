4eb6ab95649aa9ba8fc53b994f1d04b9e6e185b3
rennmaus-coder <christian@palm-family.de>
2022-11-27 17:20:24 +0100

    delete not needed files for driver

--- NINA.Plugin.Template/Dockable/FocusControlVM.cs
+++ NINA.Plugin.Template/Dockable/FocusControlVM.cs
@@ -35,7 +35,6 @@ namespace LensAF.Dockable
     [Export(typeof(IDockableVM))]
     public class FocusControlVM : DockableVM
     {
-        private List<string> Issues;
         private ICameraMediator Camera;
         private bool _manualFocusControl = false;
         private CancellationTokenSource FocusControlToken;
@@ -84,11 +83,11 @@ namespace LensAF.Dockable
             ImageGeometry = (GeometryGroup)dict["PluginSVG"];
             ImageGeometry.Freeze();
 
-            Issues = new List<string>();
 
             StartFocusControl = new AsyncCommand<bool>(async _ =>
             {
-                if (!Validate())
+                List<string> Issues = Utility.Validate(cam);
+                if (Issues.Count > 0)
                 {
                     foreach (string issue in Issues)
                     {
@@ -147,31 +146,6 @@ namespace LensAF.Dockable
             {
                 EDSDK.EdsSendCommand(Utility.GetCamera(Camera), EDSDK.CameraCommand_DriveLensEvf, (int)EDSDK.EvfDriveLens_Near2);
             });
-        }
-
-        private bool Validate()
-        {
-            Issues.Clear();
-            bool cameraConnected = Camera.GetInfo().Connected;
-
-            if (!cameraConnected)
-            {
-                Issues.Add("Camera not connected");
-            }
-
-            if (LensAFVM.Instance.AutoFocusIsRunning)
-            {
-                Issues.Add("Can't enable focus control when AF is running");
-            }
-
-            CameraVM cameraVM = (CameraVM)Utility.GetInstanceField((CameraMediator)Camera, "handler");
-
-            if (cameraVM.DeviceChooserVM.SelectedDevice.Category != "Canon")
-            {
-                Issues.Add("No canon camera connected");
-            }
-
-            return !(Issues.Count > 0);
         }
     }
 }
